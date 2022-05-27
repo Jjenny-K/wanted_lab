1adf0fb6b6c6f61a04f7b0b6dcefe8fe7afd99bc
Jjenny-K <kkjjh0129@gmail.com>
2022-05-27 18:27:04 +0900

    Fix: db_uploader.py system setup 수정

--- companies/utils/db_uploader.py
+++ companies/utils/db_uploader.py
@@ -5,7 +5,7 @@ import csv
 
 # system setup
 sys.path.append((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'config.settings.develop')
 django.setup()
 
 from django.conf import settings
